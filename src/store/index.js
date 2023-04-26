2ba8204b30f2d3771bb284e26eddfe9f44b7f576
Oscar <oscar.galindo1@yahoo.com>
2023-04-26 10:04:44 -0600

    --amend

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
 import { createStore } from 'vuex';
-import journalModule from '@/modules/daybook/store/journal';
+import journal from '@/modules/daybook/store/journal';
 
 const store = createStore({
 	modules: {
-		journalModule,
+		journal,
 	},
 });
 
